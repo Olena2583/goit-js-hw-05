1e8287fc1dc1960165eab164f39e78091d0e0e75
Olena2583 <e.sorochan@o2.pl>
2024-10-13 12:38:04 +0200

    fix task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 `use strict`;
 
 const sortByDescendingFriendCount = users => {
-  return users.slice().sort((a, b) => b.friends.length - a.friends.length);
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
 };
 console.log(
   sortByDescendingFriendCount([
